69ded223b26a7148df422c62eafd444cdbee1419
PAVITHRAKANNAN-ME <kannanpavithra67@gmail.com>
2024-07-09 11:04:58 +0530

    Update 7.Binary_encoder_4_2.v

--- assignment_1/7.Binary_encoder_4_2.v
+++ assignment_1/7.Binary_encoder_4_2.v
@@ -1,6 +1,12 @@
 module binary_encoder(
   input [3:0] D,
-  output [1:0] y); 
-  assign y[1] = D[2] | D[3] ;
-  assign y[0] = D[1] | D[3] ;
+  output reg [1:0] y); 
+  always @(*)begin
+    case (D)
+    1 : y = 0; 
+    2 : y  = 1; 
+    4 : y  = 2; 
+    8 : y  = 3; 
+endcase
+  end
 endmodule
